6ba123fa68a3b23353b2c93c5769a66116480afd
miraclewhips <bigmiraclewhips@gmail.com>
2022-12-31 18:02:56 +1300

    added spacebar next round script

---
+++ geoguessr-spacebar-next-round.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         GeoGuessr Spacebar Next Round
+// @description  Press spacebar to quickly start next round, or restart next game etc
+// @version      1.0
+// @author       miraclewhips
+// @match        *://*.geoguessr.com/*
+// @icon         https://www.google.com/s2/favicons?domain=geoguessr.com
+// @grant        none
+// @copyright    2022, miraclewhips (https://github.com/miraclewhips)
+// @license      MIT
+// @downloadURL    https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-spacebar-next-round.user.js
+// @updateURL    https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-spacebar-next-round.user.js
+// ==/UserScript==
+
+const validPaths = ['maps', 'game', 'country-streak', 'us-state-streak', 'daily-challenges', 'challenge'];
+
+document.addEventListener('keypress', e => {
+	if (e.code === 'Space') {
+		const path = document.location.pathname;
+		if (validPaths.every(p => !RegExp(`\/${p}.*`).test(path))) return;
+
+		if(document.activeElement.tagName === 'INPUT') return;
+		
+		const button = document.querySelector('button[class*="button_variantPrimary"]');
+		if (button && !button.parentNode.classList.contains('guess-map__guess-button')) {
+			e.preventDefault();
+			button.click();
+		}
+	}
+});
