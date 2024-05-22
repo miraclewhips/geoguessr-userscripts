6c93d547fe72d3afe892361e1869e7b73f311b1e
miraclewhips <bigmiraclewhips@gmail.com>
2024-05-22 21:59:46 +1200

    updated event framework

--- geoguessr-island-streak.user.js
+++ geoguessr-island-streak.user.js
@@ -7,9 +7,9 @@
 // @icon         https://www.google.com/s2/favicons?domain=geoguessr.com
 // @grant        unsafeWindow
 // @run-at       document-start
-// @require      https://miraclewhips.dev/geoguessr-event-framework/geoguessr-event-framework.min.js?v=9
-// @require      https://miraclewhips.dev/geoguessr-event-framework/geoguessr-streak-framework.min.js?v=9
-// @require      https://miraclewhips.dev/geoguessr-event-framework/utils/turf.min.js?v=9
+// @require      https://miraclewhips.dev/geoguessr-event-framework/geoguessr-event-framework.min.js?v=10
+// @require      https://miraclewhips.dev/geoguessr-event-framework/geoguessr-streak-framework.min.js?v=10
+// @require      https://miraclewhips.dev/geoguessr-event-framework/utils/turf.min.js?v=10
 // @copyright    2022, miraclewhips (https://github.com/miraclewhips)
 // @license      MIT
 // @downloadURL  https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-island-streak.user.js
--- geoguessr-world-score-reference.user.js
+++ geoguessr-world-score-reference.user.js
@@ -5,7 +5,7 @@
 // @author       miraclewhips
 // @match        *://*.geoguessr.com/*
 // @run-at       document-start
-// @require      https://miraclewhips.dev/geoguessr-event-framework/geoguessr-event-framework.min.js?v=9
+// @require      https://miraclewhips.dev/geoguessr-event-framework/geoguessr-event-framework.min.js?v=10
 // @icon         https://www.google.com/s2/favicons?domain=geoguessr.com
 // @grant        unsafeWindow
 // @grant        GM_addStyle
