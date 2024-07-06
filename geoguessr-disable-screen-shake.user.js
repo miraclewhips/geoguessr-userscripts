70d2f19284d628972b1d873ba10e497d70e145c9
miraclewhips <bigmiraclewhips@gmail.com>
2024-07-06 21:49:02 +1200

    added option to disable red flash in the disable-screen-shake script

--- geoguessr-disable-screen-shake.user.js
+++ geoguessr-disable-screen-shake.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GeoGuessr Disable Screen Shake
 // @description  Disables screen shake after opponent guesses
-// @version      1.0
+// @version      1.1
 // @author       miraclewhips
 // @match        *://*.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?domain=geoguessr.com
@@ -12,6 +12,8 @@
 // @updateURL    https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-disable-screen-shake.user.js
 // ==/UserScript==
 
+const ALSO_DISABLE_RED_FLASH = false;
+
 /* ############################################################################### */
 /* ##### DON'T MODIFY ANYTHING BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ##### */
 /* ############################################################################### */
@@ -21,3 +23,11 @@ GM_addStyle(`
 		animation: none !important;
 	}
 `);
+
+if(ALSO_DISABLE_RED_FLASH) {
+	GM_addStyle(`
+		*[class*="stress-indicator_container__"] {
+			display: none !important;
+		}
+	`);
+}
