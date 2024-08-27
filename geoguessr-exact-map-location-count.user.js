e6bbdce229e1f68352c8341aacb234ad3807a37f
miraclewhips <bigmiraclewhips@gmail.com>
2024-08-27 14:27:56 +1200

    Also shows the exact plays and like count

--- geoguessr-exact-map-location-count.user.js
+++ geoguessr-exact-map-location-count.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         GeoGuessr Exact Map Location Count
 // @description  Shows the exact location count on the map page, rather than 50k, 100k, etc
-// @version      1.0
+// @version      1.1
 // @author       miraclewhips
 // @match        *://*.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?domain=geoguessr.com
 // @grant        none
-// @copyright    2022, miraclewhips (https://github.com/miraclewhips)
+// @copyright    2024, miraclewhips (https://github.com/miraclewhips)
 // @license      MIT
-// @downloadURL    https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-exact-map-location-count.user.js
+// @downloadURL  https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-exact-map-location-count.user.js
 // @updateURL    https://github.com/miraclewhips/geoguessr-userscripts/raw/master/geoguessr-exact-map-location-count.user.js
 // ==/UserScript==
 
@@ -26,16 +26,21 @@ const init = () => {
 		const mapId = path.split('/')[2];
 		if(!mapId) return;
 
-		const value = document.querySelector('div[class^="map-stats_mapStat__"]:nth-child(3) div[class^="map-stats_mapStatMetricValue__"]');
+		const value = document.querySelector('div[class^="map-stats_mapStats__"]');
 		if(!value || value.id === 'mwemlc') return;
 
 		value.id = 'mwemlc';
-
+		
 		const apiRes = await window.fetch(`https://www.geoguessr.com/api/v3/search/map?q=${mapId}`);
 		const apiData = await apiRes.json();
 		if(!apiData || !apiData.length) return;
 
-		value.textContent = apiData[0]?.coordinateCount?.toLocaleString();
+		const stats = document.querySelectorAll('div[class^="map-stats_mapStatMetricValue__"]');
+		if(stats.length < 4) return;
+
+		stats[1].textContent = apiData[0]?.numberOfGamesPlayed?.toLocaleString();
+		stats[2].textContent = apiData[0]?.coordinateCount?.toLocaleString();
+		stats[3].textContent = apiData[0]?.likes?.toLocaleString();
 	});
 
 	observer.observe(document.querySelector('#__next'), { subtree: true, childList: true });
