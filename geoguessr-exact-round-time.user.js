a4ef1837c7f8f0cc9d84f4138b91379605b8194c
miraclewhips <bigmiraclewhips@gmail.com>
2024-11-21 16:25:40 +1300

    updated for new quickplay settings

--- geoguessr-exact-round-time.user.js
+++ geoguessr-exact-round-time.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GeoGuessr Exact Round Time
 // @description  Allows you to change the round time limit in increments smaller than 10s
-// @version      1.2
+// @version      1.3
 // @author       miraclewhips
 // @match        *://*.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?domain=geoguessr.com
@@ -20,7 +20,6 @@ const createButton = (text) => {
 	let button = document.createElement('a');
 	button.setAttribute('style', 'background:#7950e5; color:white; border:none; outline:none; font:inherit; cursor:pointer; font-weight:bold; padding:5px 0; width:26px; text-decoration:none;');
 	button.innerText = text;
-
 	return button;
 }
 
@@ -31,39 +30,59 @@ const adjustTime = (t) => {
 	window.dispatchEvent(new Event('storage'));
 }
 
+const adjustTimeQuickplay = (t) => {
+	let seconds = parseInt(localStorage.getItem('quickplay-time')) ?? 0;
+	seconds = Math.max(0, seconds + t);
+	localStorage.setItem('quickplay-time', seconds);
+	window.dispatchEvent(new Event('storage'));
+}
+
+const addButtons = (label, adjustFn) => {
+	label.style.display = 'flex';
+	label.style.justifyContent = 'center';
+	label.style.alignItems = 'center';
+
+	let labelText = document.createElement('span');
+	labelText.textContent = 'Round time';
+	labelText.style.margin = '0 10px';
+
+	let buttonMinus = createButton('-');
+	let buttonPlus = createButton('+');
+
+	buttonMinus.onclick = () => {
+		adjustFn(-1);
+	}
+
+	buttonPlus.onclick = () => {
+		adjustFn(1);
+	}
+
+	label.innerHTML = '';
+	label.append(buttonMinus);
+	label.append(labelText);
+	label.append(buttonPlus);
+}
+
 const init = () => {
 	const observer = new MutationObserver(() => {
 		const mapSelectLayout = document.querySelector('div[class^="play_section__"]');
+		const timerElement = document.querySelector('div[class^="round-time-settings_roundTimeSlider__"]');
 
 		if(mapSelectLayout && !document.getElementById('has-timer-buttons')) {
 			let label = mapSelectLayout.querySelectorAll('div[class^="game-options_options__"] label[class^="game-options_option__"] div[class^="game-options_optionLabel__"]')[0];
 
 			if(label) {
 				label.id = "has-timer-buttons";
-				label.style.display = 'flex';
-				label.style.justifyContent = 'center';
-				label.style.alignItems = 'center';
+				addButtons(label, adjustTime);
+			}
+		}
 
-				let labelText = document.createElement('span');
-				labelText.textContent = 'Round time';
-				labelText.style.margin = '0 10px';
+		if(timerElement && !document.getElementById('has-timer-buttons-quickplay')) {
+			let label = timerElement.querySelector('div[class^="label_label__"]');
 
-				let buttonMinus = createButton('-');
-				let buttonPlus = createButton('+');
-
-				buttonMinus.onclick = () => {
-					adjustTime(-1);
-				}
-
-				buttonPlus.onclick = () => {
-					adjustTime(1);
-				}
-
-				label.innerHTML = '';
-				label.append(buttonMinus);
-				label.append(labelText);
-				label.append(buttonPlus);
-
+			if(label) {
+				label.id = 'has-timer-buttons-quickplay';
+				addButtons(label, adjustTimeQuickplay);
 			}
 		}
 	});
